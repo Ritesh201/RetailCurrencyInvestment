ba1609866eba157c1e30986a69272b9cc82ab94f
ritesh51489 <riteshaggrwaal@gmail.com>
2026-01-28 11:04:42 +0530

    functionality added to project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,20 +9,20 @@ public class Main {
     public static void main(String[] args) {
 
         List<Transaction> transactions = List.of(
-                new Transaction(LocalDate.of(2024,1,10), "AAPL",
+                new Transaction(LocalDate.of(2026,1,12), "TATASTEEL",
                         TransactionType.BUY, 10, new BigDecimal("150"), "EUR"),
-                new Transaction(LocalDate.of(2024,1,15), "AAPL",
+                new Transaction(LocalDate.of(2026,1,15), "TATASTEEL",
                         TransactionType.SELL, 5, new BigDecimal("155"), "EUR"),
-                new Transaction(LocalDate.of(2024,1,20), "TSLA",
-                        TransactionType.SELL, 3, new BigDecimal("700"), "GBP")
+                new Transaction(LocalDate.of(2026,1,22), "RELIANCE",
+                        TransactionType.BUY, 3, new BigDecimal("700"), "INR")
         );
 
         List<ExchangeRate> rates = List.of(
-                new ExchangeRate(LocalDate.of(2024,1,10), "EUR",
+                new ExchangeRate(LocalDate.of(2026,1,12), "EUR",
                         new BigDecimal("1.10")),
-                new ExchangeRate(LocalDate.of(2024,1,15), "EUR",
+                new ExchangeRate(LocalDate.of(2026,1,15), "EUR",
                         new BigDecimal("1.12")),
-                new ExchangeRate(LocalDate.of(2024,1,20), "GBP",
+                new ExchangeRate(LocalDate.of(2026,1,22), "INR",
                         new BigDecimal("1.30"))
         );
 
